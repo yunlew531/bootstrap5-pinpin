be4586dd89a50ad690a35fc8242f3188621864a8
yunlew531 <yunlew531@gmail.com>
2021-06-10 03:17:50 +0800

    [ADD] 新增header + 主產品區

--- README.md
+++
@@ -1,3 +0,0 @@
-# Bootstrap5 + Vite
-
-https://yunlew531.github.io/bootstrap5-pinpin/
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/js/bootstrap.min'
-
-createApp(App).mount('#app')
